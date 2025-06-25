c02ec4a742daa2cf01312186e6ef2bc1a5776053
k-paris <paris.krzysztof@gmail.com>
2025-06-25 16:25:25 +0200

    add sys argv

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import sys
 from stats import count_words, count_characters, sort_characters
 
 def get_book_text(filepath):
@@ -24,7 +24,11 @@ def print_report(sorted_characters, filepath, num_words):
 
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     if book_text:
         num_words = count_words(book_text)
